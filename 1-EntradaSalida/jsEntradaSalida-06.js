29ddc4a0319c5444571127847056ede3aae5a4d4
Fernando <fernandofcaputo@gmail.com>
2020-07-16 22:56:32 -0300

    Modificado desde consola

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,19 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var vNum1;
+	var vNum2;
+	var	suma;
+
+	vNum1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	vNum2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	
+
+	suma = vNum1 + vNum2;
+
+	alert("La suma es: " + suma);
+
 }
+
 
