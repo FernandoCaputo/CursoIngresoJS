0602c18fdbe37208f12d29dcaae7a68af7c1fdee
Fernando <fernandofcaputo@gmail.com>
2020-07-16 22:28:50 -0300

    Modificado desde consola

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -4,6 +4,7 @@ y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
     var vNombre;
-    vNombre = prompt("Nombre", "Ingrese un Nombre"); //asignando prompt a vNombre
+    vNombre = prompt("Ingrese un Nombre"); //asignando prompt a vNombre
     alert(vNombre);
+
 }
