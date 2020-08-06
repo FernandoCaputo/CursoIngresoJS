297bf8d9c4b45f8a8e944bbd81a32d9389e40754
Fernando <fernandofcaputo@gmail.com>
2020-08-05 22:29:34 -0300

    modificado desde consola

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -3,19 +3,37 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
 function mostrar()
 {	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
+// var banderaDelPrimero;
+let	num;
+let	max;
+let min;
+let resp;
+
+//iniciar variables
+//banderaDelprimero="es el primero";
+
+resp='s';
+
+num = parseInt(prompt("Ingrese un numero: "));
+min = num;
+max = num;
+resp = prompt("Ingresa otro? s/n");
+
+
+while(resp=='s')
+{
+	num = parseInt(prompt("Ingrese un numero: "));
+	if(num<min)
 	{
-		
-		respuesta=prompt("desea continuar?");
+		min=num;
+	}else if(num>max){
+		max=num;
 	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
+	resp= prompt("Ingresa otro? s/n");
+}
+
+document.getElementById("txtIdMaximo").value = max;
+document.getElementById("txtIdMinimo").value = min;
+
+
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -12,18 +12,85 @@ hasta que el usuario quiera, mostrar:
 9-Diferencia entre positivos y negativos, (positvos-negativos). */
 function mostrar()
 {
-	//declarar contadores y variables 
-	var respuesta;
-	var numeroIngresado;
-	var sumaNegativos=0;
 
-	respuesta="si";
+	let numero;
+	let sumNeg = 0;
+	let sumPos = 0;
+	let cantPos = 0;
+	let cantNeg = 0;
+	let cantCeros = 0;
+	let cantPares = 0;
+	let promPos = 0;
+	let promNeg = 0;
+	let diferencia;
+	let respuesta;
 
-	while(respuesta=="si")
-	{
+	do {
+
+		numero = parseInt ( prompt (" Ingrese un numero : "));
 		
-		respuesta=prompt("desea continuar?");
-	}//fin del while
+		// ----------------------------SIGNOS --------------------------------------------------- 
+		if ( numero > 0 ){
 
-	document.write("la suma de negativos es :"+sumaNegativos);
-}//FIN DE LA FUNCIÓN
+			sumPos = sumPos + numero;
+
+			cantPos++;
+
+		}
+		else if ( numero < 0 ) {
+
+			sumNeg = sumNeg + numero;
+
+			cantNeg++;
+
+		}
+		else { 
+
+			cantCeros++;
+
+		}
+
+		//----------------------------------- NUMEROS PARES -------------------------------------------------
+
+		if (numero % 2 == 0 ) {
+
+			cantPares++;
+
+		}
+
+		respuesta = prompt (" Desea continuar ? : si/no");
+		
+	}
+
+
+
+	while(respuesta == "si");
+
+	if ( sumPos != 0 ){
+
+		promPos = sumPos / cantPos ;
+
+	}
+	
+	if ( sumNeg != 0 ){
+
+		promNeg = sumNeg / cantNeg; 
+	}
+
+	diferencia = sumPos - sumNeg;
+	
+
+
+	document.write ( "1-Suma de los negativos. " + sumNeg + "<br>");
+	document.write ( "2-Suma de los positivos. " + sumPos + "<br>");
+	document.write ( "3-Cantidad de positivos.  " + cantPos + "<br>");
+	document.write ( "4-Cantidad de negativos.  " + cantNeg + "<br>");
+	document.write ( "5-Cantidad de ceros.  " + cantCeros + "<br>");
+	document.write ( "6-Cantidad de números pares. " + cantPares + "<br>");
+	document.write ( "7-Promedio de positivos. " + promPos + "<br>");
+	document.write ( "8-Promedios de negativos. " + promNeg + "<br>");
+	document.write ( "9-Diferencia entre positivos y negativos. " + diferencia + "<br>");
+
+
+ }
+//FIN DE LA FUNCIÓN
