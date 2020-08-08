c816029b25fc3c36a9024ddfc65e7b5cd1f2573e
Fernando <fernandofcaputo@gmail.com>
2020-08-07 23:02:12 -0300

    Modificado desde consola

--- 6-InstruccionFor/jsIteracionesFor-07.js
+++ 6-InstruccionFor/jsIteracionesFor-07.js
@@ -1,7 +1,15 @@
-function mostrar()
-{
+function mostrar() {
 
-	alert("ok");
+	let divisores;
+	let numero;
 
+	numero = prompt("Ingrese un número");
+	for (let i = 0; i != numero; i++) {
+		if (numero % i == 0) {
+			alert("El número " + i, " es un divisor de " + numero);
+			divisores++;
+		}
+	}
+	alert("La cantidad de divisores es de "+ divisores);
 
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -1,7 +1,23 @@
-function mostrar()
-{
+function mostrar() {
+	let num;
+	let div = 0;
 
+	do {
+		num = prompt("Ingrese número");
+	}
+	while (num <= 1 || isNaN(num));
+	for (let i = 2; i < num; i++) {
 
-	alert("ok");
+	} if (num % i == 0 && i > div); 
+	{
+		div++;
+	}
+}
+if (div >= 1) {
+	alert(num + "No es primo");
+}
+else {
+	alert(num + "Es primo");
+}
 
-}//FIN DE LA FUNCIÓN
+//FIN DE LA FUNCIÓN
